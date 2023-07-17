2a872986ad1b84e999d74aecdc4012de02c93318
Sonicbird21 <67420120+Sonicbird21@users.noreply.github.com>
2023-07-17 19:57:16 +0200

    added a Both option (Do not look at the code!!)

--- verticalDisplayNameGeneratorZR.py
+++ verticalDisplayNameGeneratorZR.py
@@ -16,13 +16,15 @@ print("Sonic's Vertical name Generator v.2.1")
 print("-" * 97)
 print("""Options: 
       1. EOL Method
-      2. Line Seperator Method""")
+      2. Line Seperator Method
+      3. Both""")
 
-
-key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "SOFTWARE\\Yang Liu\\Zombs Royale", 0, winreg.KEY_ALL_ACCESS)
 
 def setDisplayName(name):
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "SOFTWARE\\Yang Liu\\Zombs Royale", 0, winreg.KEY_ALL_ACCESS)
     winreg.SetValueEx(key, "displayName_h2033418264", 0, winreg.REG_BINARY, name)
+    key.Close()
+
 
 while True:
     try:
@@ -50,6 +52,21 @@ while True:
             print(f"Your name has been updated to {name_input}" + " using the Line Seperator method. \n(Note: This method significantly reduces the amount of characters you can use in your name.)")
 
             break
+        elif method == 3:
+            name_input = input("Enter your display name: ")
+
+            ls_char = b"\xE2\x80\xA8"
+            displayName = b""
+
+            for i, char in enumerate(name_input.encode('utf-8')):
+                displayName += bytes([char])
+                if i < len(name_input) - 1:
+                    displayName += ls_char
+            
+            setDisplayName(displayName + b"\x03")
+            print(f"Your name has been updated to {name_input}" + " using both methods. \n(Note: The Line Separator method significantly reduces the amount of characters you can use in your name.)")
+            break
+
         else:
             print("Invalid input, please try again.")
     except ValueError:
